7497384b7a81f95ed7c2e7982669054986faed6e
LauraB <116919841+qa-tester22@users.noreply.github.com>
2024-04-30 13:19:48 +0000

    first

---
+++ 1_countdown.js
@@ -0,0 +1,14 @@
+let i;
+function countdown(i) {
+   
+
+//  if (i <= 0) {
+//        return (i); 
+//     } else 
+   {
+       console.log(i);
+    return ( countdown (i - 1))
+    }
+}
+
+console.log(countdown(10))
